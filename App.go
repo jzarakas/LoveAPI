34329f28c5bb99e2b15653c15ed52f335beb0333
Abdullah Abdurrah <asahyoun@gmu.edu>
2015-02-08 01:06:05 -0500

    imiplimented get next

--- App.go
+++ App.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -21,13 +23,14 @@ type responseNode struct {
 }
 
 var queue = make([]queuedSong, 100)
+var queLen int64 = 0
 
 func main() {
 
 	seedData()
 
-	http.HandleFunc("/api", welcome)
-	//http.HandleFunc("/get"
+	http.HandleFunc("/api/", welcome)
+	http.HandleFunc("/getNext/", getNext)
 	http.ListenAndServe(":8000", nil)
 
 }
@@ -48,6 +51,28 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 }
 
 func getNext(w http.ResponseWriter, r *http.Request) {
+	//strings.SplitN(r.URL.Path, "/", 3)[2]
+	//fmt.Printf("%v", r.URL.Path)
+
+	key := strings.Split(r.URL.Path, "/")[2]
+
+	num, err := strconv.ParseInt(key, 10, 0)
+
+	fmt.Printf("%v", queue[num+1])
+
+	if err != nil || num+1 > queLen {
+		return
+	}
+
+	zero, _ := time.ParseDuration("0m")
+	var res = responseNode{Que: queue[num+1].Que, SongId: queue[num].SongId, StartAt: zero}
+	js, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 
 }
 
@@ -58,4 +83,6 @@ func seedData() {
 	durTwo, _ := time.ParseDuration("10m")
 	queue[1] = queuedSong{Que: 1, SongId: "songTwo", StartTime: queue[0].StartTime.Add(queue[0].TimeDuration), TimeDuration: durTwo}
 	queue[2] = queuedSong{Que: 2, SongId: "songThree", StartTime: queue[1].StartTime.Add(queue[1].TimeDuration), TimeDuration: durOne}
+
+	queLen = 3
 }
